34c2660da93ed276c63253f99d51d4c110cb3169
badgerbricker <muschott@wisc.edu>
2020-04-16 12:08:01 -0700

    aruco marker detection working

--- src/final_project/src/test_camera.py
+++ src/final_project/src/test_camera.py
@@ -4,16 +4,24 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+parameters = cv2.aruco.DetectorParameters_create()
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    markercorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame,dictionary,parameters=parameters)
+ 
+    if(len(markercorners)>0):
+    	cv2.aruco.drawDetectedMarkers(frame, markercorners, markerIds)
+	    #for rect in markercorners:
+	    #	cv2.rectangle(frame,rect[0],rect[2],(0,0,255),15)
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
