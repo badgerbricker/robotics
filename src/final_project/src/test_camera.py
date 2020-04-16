33af7ccf2fc43312fb19faff9cece851ae45952e
badgerbricker <muschott@wisc.edu>
2020-04-16 15:29:36 -0700

    working tracking for hand, elbow, shoulder scaled to real world

--- src/final_project/src/test_camera.py
+++ src/final_project/src/test_camera.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python
 
+
+
 import numpy as np
 import cv2
+
+
+def find_center(corner):
+	x_mid=corner[0][0][0]
+	x_mid+=abs(corner[0][0][0]-corner[0][1][0])
+
+	y_mid=corner[0][0][1]
+	y_mid+=abs(corner[0][0][1]-corner[0][3][1])
+	return x_mid,y_mid
+
+def find_world_frame(pixel_x,pixel_y,frame):
+	#adjustment assumes person stands under (0,0)
+	#this is usually right side of frame left arm
+	dimensions=frame.shape
+	height=dimensions[0]
+	width=dimensions[1]
+	print("Height: "+str(height))
+	print("Width: "+str(width))
+
+	x_frame=(pixel_x/width)*1700 #estimated mm distance
+	y_frame=(pixel_y/height)*1350 #mm distance
+
+	return x_frame,y_frame
+
+#def scale_to_robot_arm(x_shoulder, y_shoulder, x_hand, y_hand, frame):
+#	return None
 
 cap = cv2.VideoCapture(0)
 dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
@@ -15,11 +43,27 @@ while(True):
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     markercorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame,dictionary,parameters=parameters)
+    #print(markercorners)
+    #print()
  
     if(len(markercorners)>0):
     	cv2.aruco.drawDetectedMarkers(frame, markercorners, markerIds)
-	    #for rect in markercorners:
-	    #	cv2.rectangle(frame,rect[0],rect[2],(0,0,255),15)
+    	corner_num=0
+    	while corner_num < len(markercorners):
+    		if(markerIds[corner_num]==10):
+    			X,Y=find_center(markercorners[corner_num])
+    			x,y=find_world_frame(X,Y,frame)
+    			print("Shoulder: "+str(x)+","+str(y)+" mm")
+    		if(markerIds[corner_num]==20):
+    			X,Y=find_center(markercorners[corner_num])
+    			x,y=find_world_frame(X,Y,frame)
+    			print("Elbow: "+str(x)+","+str(y)+" mm")
+    		if(markerIds[corner_num]==30):
+    			X,Y=find_center(markercorners[corner_num])
+    			x,y=find_world_frame(X,Y,frame)
+    			print("Hand: "+str(x)+","+str(y)+" mm")
+    		corner_num+=1
+
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
